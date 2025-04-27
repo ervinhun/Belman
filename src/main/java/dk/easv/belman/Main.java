07f8554d65016bf6b8636b5c3d80b497f76a0c78
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-04-27 16:14:13 +0200

    Trying some stuff

--- src/main/java/dk/easv/belman/Main.java
+++ src/main/java/dk/easv/belman/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FXML/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Order System");
+        stage.setTitle("Order System2");
         stage.setScene(scene);
         stage.show();
     }
