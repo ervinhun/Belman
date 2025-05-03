994ee27264d17fa12386b85d5d43582fd8e290f8
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-05-03 18:43:35 +0200

    23C - signing quality document

---
+++ src/main/java/dk/easv/belman/be/QualityDocument.java
@@ -0,0 +1,64 @@
+package dk.easv.belman.be;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class QualityDocument {
+    private long id;
+    private UUID generatedBy;
+    private long productId;
+    private LocalDateTime generatedAt;
+    private String qcDocPath;
+
+    public QualityDocument(long id, UUID generatedBy, long productId, LocalDateTime generatedAt, String qcDocPath) {
+        this.id = id;
+        this.generatedBy = generatedBy;
+        this.productId = productId;
+        this.generatedAt = generatedAt;
+        this.qcDocPath = qcDocPath;
+    }
+    public QualityDocument(UUID generatedBy, long productId) {
+        this.generatedBy = generatedBy;
+        this.productId = productId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public UUID getGeneratedBy() {
+        return generatedBy;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public LocalDateTime getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public String getQcDocPath() {
+        return qcDocPath;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setGeneratedBy(UUID generatedBy) {
+        this.generatedBy = generatedBy;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public void setGeneratedAt(LocalDateTime generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+
+    public void setQcDocPath(String qcDocPath) {
+        this.qcDocPath = qcDocPath;
+    }
+}
