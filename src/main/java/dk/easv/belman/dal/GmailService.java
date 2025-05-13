54c5b43dbd590375cf000fa63d95e29f9f6a3de1
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-05-13 18:29:21 +0200

    mailto v2

---
+++ src/main/java/dk/easv/belman/dal/GmailService.java
@@ -0,0 +1,86 @@
+package dk.easv.belman.dal;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.model.Message;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class Gmail {
+    private static final String APPLICATION_NAME = "GmailFXSender";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Collections.singletonList("https://www.googleapis.com/auth/gmail.send");
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private Gmail service;
+
+    public void GmailService() throws Exception {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        InputStream in = getClass().getResourceAsStream("/credentials.json");
+        if (in == null) {
+            throw new FileNotFoundException("credentials.json not found");
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                new com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp(
+                        flow, new com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver()).authorize("user"))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public void sendEmailWithAttachment(String userId, String to, String subject,
+                                        String bodyText, File file) throws Exception {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(userId));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setText(bodyText);
+
+        MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+        attachmentBodyPart.setDataHandler(new DataHandler(new FileDataSource(file)));
+        attachmentBodyPart.setFileName(file.getName());
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+        multipart.addBodyPart(attachmentBodyPart);
+        email.setContent(multipart);
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = com.google.api.client.util.Base64.encodeBase64URLSafeString(rawMessageBytes);
+
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+
+        service.users().messages().send(userId, message).execute();
+    }
+}
