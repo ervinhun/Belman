358a744a7532d8dc0afce19f95466b4d9591406f
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-05-15 09:36:14 +0200

    generating report fix

--- src/main/java/dk/easv/belman/dal/OpenFile.java
+++ src/main/java/dk/easv/belman/dal/OpenFile.java
@@ -1,21 +1,23 @@
 package dk.easv.belman.dal;
 
+import dk.easv.belman.exceptions.BelmanException;
+
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class OpenFile {
     public OpenFile(String filePath) {
-        //File pdfFile = new File(FilePaths.BASE_PATH + "report/" + productNumber + "/report.pdf");
         File fileToOpen = new File(filePath);
         if (Desktop.isDesktopSupported() && fileToOpen.exists()) {
             try {
                 Desktop.getDesktop().open(fileToOpen);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new BelmanException("Error getting the file. " + e);
             }
         } else {
-            System.out.println("Desktop is not supported on this system.");
+            throw new BelmanException("Desktop is not supported on this system.");
         }
     }
 
@@ -25,10 +27,29 @@ public class OpenFile {
             try {
                 Desktop.getDesktop().open(fileToOpen);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new BelmanException("Desktop is not supported to open the file. " + e);
             }
         } else {
-            System.out.println("Desktop is not supported on this system.");
+            throw new BelmanException("Desktop is not supported on this system.");
+        }
+        if (isSendingEmail && fileToOpen.exists()) {
+
+            GmailService gmailService = null;
+            try {
+                gmailService = new GmailService();
+            } catch (GeneralSecurityException | IOException e) {
+                throw new BelmanException(e.getMessage());
+            }
+            try {
+                if (email != null && !email.isEmpty()) {
+                    gmailService.sendEmailWithAttachment("me", "nyeres@gmail.com",
+                            "Quality Check Report", "Please find the attached report.", fileToOpen.getAbsoluteFile());
+                } else {
+                    throw new BelmanException("Email address is null or empty.");
+                }
+            } catch (Exception e) {
+                throw new BelmanException("Error while trying to send an e-mail: " + e);
+            }
         }
     }
 }
